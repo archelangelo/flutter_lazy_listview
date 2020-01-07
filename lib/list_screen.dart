e2bcae32b86c7d2dfa6e8b8371947d810743329d
Yiran Hu <yiranhu@gatech.edu>
2020-01-06 23:43:46 -0500

    Tutorial starting point.

---
+++ lib/list_screen.dart
@@ -0,0 +1,44 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+class ListScreen extends StatefulWidget {
+  @override
+  _ListScreenState createState() => _ListScreenState();
+}
+
+class _ListScreenState extends State<ListScreen> {
+  final _pairList = <WordPair>[WordPair.random()];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: _pairList.length,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          leading: Text(index.toString(), style: _biggerFont),
+          title: Text(_pairList[index].asPascalCase, style: _biggerFont),
+        );
+      },
+    );
+  }
+}
+
+class _ItemFetcher {
+  final _count = 103;
+  final _itemsPerPage = 5;
+  int _currentPage = 0;
+
+  // This async function simulates fetching results from Internet, etc.
+  Future<List<WordPair>> fetch() async {
+    final list = <WordPair>[];
+    final n = _count - _currentPage * _itemsPerPage;
+    await Future.delayed(Duration(seconds: 1), () {
+      for (int i = 0; i < n; i++) {
+        list.add(WordPair.random());
+      }
+    });
+    _currentPage++;
+    return list;
+  }
+}
