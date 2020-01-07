596952919d11e7dfad40d66e8b2554d2ac167b93
Yiran Hu <yiranhu@gatech.edu>
2020-01-07 00:21:07 -0500

    Fixed bug in fetch()

--- lib/list_screen.dart
+++ lib/list_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
@@ -34,7 +36,9 @@ class _ItemFetcher {
   // This async function simulates fetching results from Internet, etc.
   Future<List<WordPair>> fetch() async {
     final list = <WordPair>[];
-    final n = _count - _currentPage * _itemsPerPage;
+    final n = min(_itemsPerPage, _count - _currentPage * _itemsPerPage);
+    // Uncomment the following line to see in real time now items are loaded lazily.
+    // print('Now on page $_currentPage');
     await Future.delayed(Duration(seconds: 1), () {
       for (int i = 0; i < n; i++) {
         list.add(WordPair.random());
