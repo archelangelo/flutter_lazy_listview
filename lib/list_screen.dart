0c7dfd1c2b9825187fb5d48eaa2d944bf67e4d3c
Yiran Hu <yiranhu@gatech.edu>
2020-01-07 00:28:45 -0500

    Completed app

--- lib/list_screen.dart
+++ lib/list_screen.dart
@@ -9,16 +9,58 @@ class ListScreen extends StatefulWidget {
 }
 
 class _ListScreenState extends State<ListScreen> {
-  final _pairList = <WordPair>[WordPair.random()];
+  final _pairList = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
   final _itemFetcher = _ItemFetcher();
+
+  bool _isLoading = true;
+  bool _hasMore = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _isLoading = true;
+    _hasMore = true;
+    _loadMore();
+  }
+
+  void _loadMore() {
+    _isLoading = true;
+    _itemFetcher.fetch().then((List<WordPair> fetchedList) {
+      if (fetchedList.isEmpty) {
+        setState(() {
+          _isLoading = false;
+          _hasMore = false;
+        });
+      } else {
+        setState(() {
+          _isLoading = false;
+          _pairList.addAll(fetchedList);
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: _pairList.length,
+      // Need to display a loading tile if more items are coming
+      itemCount: _hasMore ? _pairList.length + 1 : _pairList.length,
       itemBuilder: (BuildContext context, int index) {
-        // TODO: Display the list items and load more when needed
+        // Uncomment the following line to see in real time how ListView.builder works
+        // print('ListView.builder is building index $index');
+        if (index >= _pairList.length) {
+          if (!_isLoading) {
+            _loadMore();
+          }
+          return Center(
+            child: SizedBox(
+              child: CircularProgressIndicator(),
+              height: 24,
+              width: 24,
+            ),
+          );
+        }
         return ListTile(
           leading: Text(index.toString(), style: _biggerFont),
           title: Text(_pairList[index].asPascalCase, style: _biggerFont),
